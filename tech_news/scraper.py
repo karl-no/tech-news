35e2913fbb600da111e958d02a26fad5e8fa87b6
karl-no <karl.publicidade@gmail.com>
2023-04-13 16:04:05 -0300

    feat: Function fetch created

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,23 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """Seu código deve vir aqui"""
+    time.sleep(1)
+
+    try:
+        response = requests.get(
+            url,
+            headers={"user-agent": "Fake user-agent"},
+            timeout=3,
+        )
+        if response.status_code == 200:
+            return response.text
+        else:
+            return None
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
