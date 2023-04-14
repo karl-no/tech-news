738ca04fe866769c16ace5933c63f6ca268f2e5d
karl-no <karl.publicidade@gmail.com>
2023-04-14 10:58:07 -0300

    feat: function top_5_categories concluded

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,20 @@
+from tech_news.database import find_news
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    categories = []
+
+    for info in news:
+        categories.append(info["category"])
+
+    categories_counter = Counter(
+        sorted(categories)
+    ).most_common(5)
+
+    most_popular_categories = []
+    for category in categories_counter:
+        most_popular_categories.append(category[0])
+    return most_popular_categories
