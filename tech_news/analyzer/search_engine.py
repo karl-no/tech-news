586b1d5d29f42a1d39b2dcb58e6d10f110b64174
karl-no <karl.publicidade@gmail.com>
2023-04-14 10:17:59 -0300

    feat: fucntion search_by_date concluded

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import date as data
 
 
 # Requisito 7
@@ -20,7 +21,20 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        news = search_news({
+            "timestamp": data.fromisoformat(date).strftime("%d/%m/%Y")
+        })
+        list_of_news = []
+
+        for item in news:
+            info = item["title"], item["url"]
+            list_of_news.append(info)
+
+        return list_of_news
+
+    except ValueError:
+        raise ValueError("Data inválida")
 
 
 # Requisito 9
