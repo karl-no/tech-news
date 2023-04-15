aedc29b2dbd4579f78a2a7c34605c892f63be441
karl-no <karl.publicidade@gmail.com>
2023-04-15 19:25:40 -0300

    feat: sketch of the analyzer_menu function set, satisfacting requirement 11

--- tech_news/menu.py
+++ tech_news/menu.py
@@ -1,3 +1,9 @@
 # Requisitos 11 e 12
 def analyzer_menu():
-    """Seu código deve vir aqui"""
+    print("Selecione uma das opções a seguir:\n"
+          " 0 - Popular o banco com notícias;\n"
+          " 1 - Buscar notícias por título;\n"
+          " 2 - Buscar notícias por data;\n"
+          " 3 - Buscar notícias por categoria;\n"
+          " 4 - Listar top 5 categorias;\n"
+          " 5 - Sair.")
